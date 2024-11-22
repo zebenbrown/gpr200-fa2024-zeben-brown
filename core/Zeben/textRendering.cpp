97b444e3a6ce1f8b4f26da1d106ecf7bcd9d8bdb
zebenbrown <zeben.brown@mymail.champlain.edu>
2024-11-22 01:23:15 -0500

    tried to add freetype library

---
+++ core/Zeben/textRendering.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zeben on 11/20/2024.
+//
+
+#include "textRendering.h"
---
+++ core/Zeben/textRendering.h
@@ -0,0 +1,17 @@
+//
+// Created by Zeben on 11/20/2024.
+//
+
+#ifndef EWRENDER_TEXTRENDERING_H
+#define EWRENDER_TEXTRENDERING_H
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+
+class textRendering{
+
+};
+
+
+#endif //EWRENDER_TEXTRENDERING_H
